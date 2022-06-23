e4ef3a93e24213bba5199257b0e0f1dc8ab37981
Alexey <a.d.kamenev@yandex.ru>
2022-06-23 09:31:04 +0300

    homework12 v0.1

---
+++ src/main/java/Array.java
@@ -0,0 +1,10 @@
+public class Array {
+    static final int SIZE = 10000000;
+    static final int HALF = SIZE / 2 ;
+
+    public static void main(String[] args) {
+    float[] arr = new float[SIZE];
+    }
+
+
+}
